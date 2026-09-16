2b84ef3c0c3bf30fa710ec16478dbb38beaa79f2
Dre Santos <dre.santoz@gmail.com>
2026-09-16 13:12:05 -0300

    New book revisions

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'maeri-rpg-v4';
+const CACHE_NAME = 'maeri-rpg-v5';
 
 // Detecta automaticamente se está no GitHub Pages
 const BASE_PATH = self.location.pathname.includes('/maeri/') 
